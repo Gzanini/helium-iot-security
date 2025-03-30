63aa72486164b861ba9bad52b418d12d6cea4b83
gzanini <guilhermezsilva@hotmail.com>
2025-03-30 02:36:34 -0300

    fix cloud functions

--- cloud/functions/save_payload_to_gcs/main.py
+++ cloud/functions/save_payload_to_gcs/main.py
@@ -48,3 +48,8 @@ def handle_request():
     except Exception as e:
         print(f"Erro inesperado: {e}")
         return "Internal error", 500
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
