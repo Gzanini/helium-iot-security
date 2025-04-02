ee6d04eca506f5e5fe146cb381723eae9fe428d6
gzanini <guilhermezsilva@hotmail.com>
2025-04-02 12:52:13 -0300

    fix join_OTAA_test

--- tests/firmware/join_OTAA_test/join_OTAA_test.ino
+++ tests/firmware/join_OTAA_test/join_OTAA_test.ino
@@ -2,9 +2,12 @@
 #include <hal/hal.h>
 #include <SPI.h>
 
-static const u1_t PROGMEM APPEUI[8] = { 0xE2, 0xE4, 0x33, 0x38, 0xEC, 0x6C, 0x86, 0xB9 };
-static const u1_t PROGMEM DEVEUI[8] = { 0xB5, 0x0E, 0x32, 0x84, 0xDC, 0x46, 0x0F, 0x28 };
-static const u1_t PROGMEM APPKEY[16] = { 0xFD, 0xCC, 0x5A, 0x40, 0x8D, 0x08, 0x16, 0xC5, 0x00, 0x29, 0x55, 0xC9, 0x36, 0x57, 0x8F, 0x1F };
+#define LED_PIN_G 22
+#define LED_PIN_Y 23
+
+static const u1_t PROGMEM APPEUI[8]  = { 0x00, 0x00, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 }; // padrão TagoIO
+static const u1_t PROGMEM DEVEUI[8]  = { 0xB5, 0x0E, 0x32, 0x84, 0xDC, 0x46, 0x0F, 0x28 }; // seu DevEUI
+static const u1_t PROGMEM APPKEY[16] = { 0x77, 0xCB, 0x69, 0xBA, 0xA1, 0x00, 0x49, 0x9F,0xB4, 0x50, 0x41, 0x7C, 0xBF, 0x29, 0x6C, 0xDF }; // seu AppKey
 
 void os_getArtEui(u1_t* buf) { memcpy_P(buf, APPEUI, 8); }
 void os_getDevEui(u1_t* buf) { memcpy_P(buf, DEVEUI, 8); }
@@ -25,12 +28,18 @@ void onEvent(ev_t ev) {
   switch (ev) {
     case EV_JOINING:
       Serial.println("🔄 Tentando conectar via OTAA...");
+	  digitalWrite(LED_PIN_G, LOW);
+	  digitalWrite(LED_PIN_Y, LOW);
       break;
     case EV_JOINED:
       Serial.println("✅ Conectado à rede LoRaWAN via OTAA!");
+	  digitalWrite(LED_PIN_G, HIGH);
+	  digitalWrite(LED_PIN_Y, LOW);
       break;
     case EV_JOIN_FAILED:
       Serial.println("❌ Falha na conexão OTAA.");
+	  digitalWrite(LED_PIN_G, LOW);
+	  digitalWrite(LED_PIN_Y, HIGH);
       break;
     default:
       Serial.print("Evento: ");
@@ -43,11 +52,16 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
   Serial.println("🔧 Iniciando LMIC para Join Test...");
+  
+  pinMode(LED_PIN_G, OUTPUT);
+  pinMode(LED_PIN_Y, OUTPUT);
+  digitalWrite(LED_PIN_G, LOW);
+  digitalWrite(LED_PIN_Y, LOW);
 
   SPI.begin(5, 16, 17, 18); // SCK, MISO, MOSI, SS
   os_init();
   LMIC_reset();
-  LMIC_selectSubBand(1);
+  LMIC_selectSubBand(1);  // <- ESSENCIAL PARA HELIUM US915
   LMIC_startJoining();
 }
 
